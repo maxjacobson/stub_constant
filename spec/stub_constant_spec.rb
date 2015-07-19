399ddf95c98033141236e169891e99f72bf2cf85
Max Jacobson <max@hardscrabble.net>
2015-07-19 15:16:58 -0400

    add a spec for namespaced constants

--- spec/stub_constant_spec.rb
+++ spec/stub_constant_spec.rb
@@ -22,4 +22,22 @@ describe StubConstant do
     StubConstant.value(:LIMIT, 4)
     expect(LIMIT).to eq 4
   end
+
+  it "can stub namespaced constants" do
+    expect { Turkey::Duck::Chicken }.to raise_error(NameError)
+    StubConstant.klass(:'Turkey::Duck::Chicken')
+    expect(Turkey).to be_a Class
+    expect(Turkey::Duck).to be_a Class
+    expect(Turkey::Duck::Chicken).to be_a Class
+  end
+
+  it "can stub namespaced constants of mixed types with a little help" do
+    expect { Earth }.to raise_error(NameError)
+    StubConstant.module(:Earth)
+    StubConstant.klass(:'Earth::Uruguay')
+    StubConstant.value(:'Earth::Uruguay::POPULATION', 3.42 * 10**6)
+    expect(Earth).to be_a Module
+    expect(Earth::Uruguay).to be_a Class
+    expect(Earth::Uruguay::POPULATION).to be_a Float
+  end
 end
